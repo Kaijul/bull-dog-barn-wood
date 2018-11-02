ec8ba4451afc3b41997d533e4126ac356b58900b
Joe Miner <JMin1987@gmail.com>
2018-11-02 16:52:10 -0500

    initial work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'bull-dog-barn-wood';
+
+  private visibleWood: number[] = [1, 2, 3];
+
+  title = 'Bull Dog Barn Wood';
+
+  IsHidden(num) {
+    return !this.visibleWood.includes(num);
+  }
+
+  leftWood() {
+    var first = this.visibleWood[0];
+    if (first === 1) {
+      return;
+    }
+
+    first--;
+    var second = first + 1;
+    var last = first + 2;
+
+    this.visibleWood = [first, second, last];
+  }
+
+  rightWood() {
+    var last = this.visibleWood[this.visibleWood.length - 1];
+    if (last === 4) {
+      return;
+    }
+
+    last++;
+    var second = last - 1;
+    var first = last - 2;
+    this.visibleWood = [first, second, last];
+  }
 }
